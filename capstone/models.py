7deced461f2c455620279af9e32c33319badd5a0
Alexander Kovchiy <script062@gmail.com>
2025-01-25 14:22:58 +0200

    fix error

--- capstone/models.py
+++ capstone/models.py
@@ -14,6 +14,7 @@ class User(AbstractUser):
     is_superuser = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
 
 
 class Profile(models.Model):
